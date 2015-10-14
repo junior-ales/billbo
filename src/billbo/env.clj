2b24ab9604479f4d1fcc8bb75a018539e3384ce0
Taina Caetano <tainacaetano@gmail.com>
2015-10-13 22:55:14 -0300

    Removes env reader function since it is not used anymore

--- src/billbo/env.clj
+++
@@ -1,7 +0,0 @@
-(ns billbo.env)
-
-(defn read-env [property-name]
-  (let [value (System/getenv property-name)]
-    (if (nil? value)
-      (throw (IllegalStateException. (str "Could not find property " property-name " in the environment")))
-      value)))
