61014fdb6285864ccac68bf3d7a01ff6291b4224
Junior Ales <edilson.ales.jr@gmail.com>
2015-10-15 23:32:08 -0300

    listing bills from DB

--- resources/app/views/components/billList.jsx
+++ resources/app/views/components/billList.jsx
@@ -5,21 +5,37 @@ var Bill = React.createClass({
     return(
       <a className="th" href="#">
         <img src="http://placehold.it/300x400?text=Conta" alt="Conta" />
+        <h2>{this.props.data["issued-by"]}</h2>
+        <p>R$ {this.props.data["total-amount"]}</p>
+        <p>Vencimento: {this.props.data["due-date"]}</p>
       </a>
     );
   }
 });
 
 var BillList = React.createClass({
-  render: function() {
-    var bills = [];
-    for (var i = 0; i < 12; i++) {
-      bills.push(<li key={'id' + i}><Bill /></li>);
-    }
+  getInitialState: function() {
+    return { bills: [] };
+  },
 
+  componentDidMount: function() {
+    var self = this;
+    var request = new XMLHttpRequest();
+    request.open('GET', encodeURI(self.props.source));
+    request.onload = function() {
+      if (self.isMounted()) {
+         self.setState({ bills: JSON.parse(request.responseText)});
+      }
+    };
+    request.send();
+  },
+
+  render: function() {
     return(
       <ul className="small-block-grid-2 medium-block-grid-3">
-        {bills}
+        {this.state.bills.map(function(bill) {
+          return <li key={"billId"+bill["bill-id"]}><Bill data={bill} /></li>;
+        })}
       </ul>
     );
   }
